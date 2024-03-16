2044789d40bb08426f545cd8d311ff1c4d31d151
Bob Khounsombath <bob.khounsombath@gmail.com>
2024-03-15 21:16:08 -0700

    added darkmode toggle for textarea confirmation div; fixed empty str category tag

---
+++ static/js/db.js
@@ -0,0 +1,63 @@
+// const db=new PouchDB('test');
+// const remoteCouch=false;
+
+const flashcardData = {
+    tags: ["math", "multiplication"],
+    question: "What is 10 * 10?",
+    answer:{
+        text:{
+            regular:"regular text.",
+            markdown:"#Markdown text",
+            latex:"\\[\\int_a^b f(\\mu) \\, d\\mu\\]",
+            mermaid:"graph LR;A[Square Rect] -- Link text --> B((Circle));A --> C(Round Rect);B --> D{Rhombus};C --> D;"
+        },
+        image:"Blob",
+        audio:"BlobURL",
+        video:{
+            local:"BlobURL",
+            youtube:"src"
+        }
+    }
+};
+
+
+const pouchDB={
+
+    data:this.data,
+
+    addFlashcard(flashCardDoc){
+        flashCardDoc=flashCardDoc||this.constructFlashcardDocument(this.data);
+        if(flashCardDoc.hasOwnProperty('_id')){
+            // db == new PouchDB('myDBInstance') from global context.
+            db.put(flashCardDoc);
+            return;
+        }
+        throw 'The flashcard document must have an _id property.'
+    },
+
+    constructFlashcardDocument(data){
+        /* Create a single flashCard document from the data entered in Flashcard Builder form.  
+        *   Save the outputted document to PouchDB.
+        */
+        this.data=data;
+        const flashCardID={_id: new Date().toISOString()};
+        const flashCardObject=Object.assign(flashCardID, data);
+        return flashCardObject;
+    },
+    toJson(spaces){
+        spaces=spaces||0;
+        return JSON.stringify(this.constructFlashcardDocument(this.data),null,spaces)
+    },
+    print(){console.dir(this.constructFlashcardDocument(this.data))},
+    pprint(spaces){
+        spaces=spaces||2;
+        console.log(this.toJson(spaces))
+    }
+};
+
+pouchDB.data=flashcardData;
+// pouchDB.constructFlashcardDocument(flashcardData);
+pouchDB.print();
+pouchDB.pprint();
+
+
