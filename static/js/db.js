0d22e46c2e785a1ad410fc5e59830b5d6b080599
Bob Khounsombath <bob.khounsombath@gmail.com>
2024-04-09 22:42:40 -0700

    added temp feature to query db by flashcard question

--- static/js/db.js
+++ static/js/db.js
@@ -1,4 +1,4 @@
-// const db=new PouchDB('test');
+const db=new PouchDB('test');
 // const remoteCouch=false;
 
 // const flashcardData = {
@@ -24,7 +24,7 @@
 //     }
 // };
 
-class Pouch {
+class Flashcard {
     static blobType = ['input-image', 'input-audio', 'input-video'];
 
     constructor(form){
@@ -33,23 +33,23 @@ class Pouch {
         /** @type {FormData} */
         this.formData = new FormData(this.form);
         /** @type {Number} */
-        this.numInputs=null;
+        this.numInputs=0;
         /** @type {Object} */
         this.doc={};
-        this.processFormData();
+        this._processFormData();
         /** @type {Object} */
         this.lastSavedCard={};
     }
 
-    processFormData(){
+    _processFormData(){
         const preDoc={};
         const attachments={};
         this.formData.delete('inlineRadioOptions');
         for (const [_key,_value] of this.formData.entries()){
-            if(Pouch.blobType.includes(_key)){
+            if(Flashcard.blobType.includes(_key)){
                 if(_value.size>0){
                     const blobFile = new Blob([_value], {type:_value.type});
-                    const keyName=`${Pouch.blobType[Pouch.blobType.indexOf(_key)].split('-')[1]}.${_value.type.split('/')[1]}`; // e.g. image.jpeg
+                    const keyName=`${Flashcard.blobType[Flashcard.blobType.indexOf(_key)].split('-')[1]}.${_value.type.split('/')[1]}`; // e.g. image.jpeg
                     attachments[keyName]={
                         "content_type":_value.type, // MIME: image/jpeg
                         "data":blobFile // Blob {size: INT, type: MIME}
@@ -72,24 +72,24 @@ class Pouch {
 
     addFlashcard(flashCardDoc){
         /** @type {Object} */
-        flashCardDoc=flashCardDoc||this.constructFlashcardDocument(this.doc);
+        flashCardDoc=flashCardDoc||this._constructFlashcardDocument(this.doc);
         if(flashCardDoc.hasOwnProperty('_id')){
             // db == new PouchDB('myDBInstance') from global context.
 
-            console.log('flashCard put request queued.');
-            // db.put(flashCardDoc)
-            //     .then(res=>{
-            //         console.log('Flashcard was saved to database.');
-            //         this.lastSavedCard=res;
-            //         console.log(res);
-            //     })
-            //     .catch(err=>console.error(err));
+            // console.log('flashCard put request queued.');
+            db.put(flashCardDoc)
+                .then(res=>{
+                    console.log('Flashcard was saved to database.');
+                    this.lastSavedCard=res;
+                    console.log(res);
+                })
+                .catch(err=>console.error(err));
             return;
         }
         throw 'The flashcard document must have an _id property; use new Date().toISOString().';
     }
 
-    constructFlashcardDocument(data){
+    _constructFlashcardDocument(data){
         /* Finalize a single flashCard document from the data entered in Flashcard Builder form
         *   by adding `_id` property to input dataset.
         */
@@ -124,3 +124,102 @@ class Pouch {
         console.log(this.toJson(spaces));
     }
 };
+
+class Query {
+    constructor(pouchDatabase){
+        this.db=pouchDatabase;
+    }
+    // https://pouchdb.com/api.html#batch_fetch
+    /*
+        Make sure that you have enabled the 
+        JavaScript > Implicit Project Config: Check JS setting in VSCode. 
+        This setting enables type checking for JavaScript files. 
+        If it’s not enabled, VSCode might not show type suggestions based 
+        on your JSDoc comments. You can find this setting in the 
+        settings.json file or in the settings UI (File > Preferences > Settings).
+    */
+    getAll(justMetaData,includeBlobs,mostRecentFirst){
+        /** @type {Boolean} */
+        justMetaData=justMetaData||false; // show actual data by default
+        /** @type {Boolean} */
+        includeBlobs=includeBlobs||false; // don't incl Blob attachments(image, audio, video) by default
+        /** @type {Boolean} */
+        mostRecentFirst=mostRecentFirst||true; // show most recent first by default
+
+        this.db.allDocs({
+            include_docs:!justMetaData,
+            binary:includeBlobs,
+            descending:mostRecentFirst,
+        }).then(docs=>{
+            console.log(docs);
+            return docs;
+        }).catch(err=>console.log(err));
+    }
+
+    async _question_async(regex, isCaseSensitive){
+        /* Use backslash to escape regex special characters.
+            e.g. `h\.t`
+
+            Find flashcards based on questions posed. 
+
+            This is a `Temporary query` && is considered very slow.
+            It is meant for debugging during development.
+
+            https://pouchdb.com/guides/queries.html#mappin-and-reducin
+        */
+        /** @type {Boolean} */
+        isCaseSensitive=isCaseSensitive||false;
+        /** @type {String} */
+        regex=isCaseSensitive?new RegExp(regex):new RegExp(regex,'i');
+
+        try{
+            const result = await db.query((doc,emit)=>{
+                if(regex.test(doc['input-question'])){
+                    emit(doc['input-question'],doc._rev);
+                }
+            },{
+                include_docs:true,
+                descending:true,
+            });
+            // console.log(result.rows);
+            console.log(result);
+            return result.rows; // Assignable w/in promise chain.
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    _question_sync(regex, isCaseSensitive){
+        /* Use backslash to escape regex special characters.
+            e.g. `h\.t`
+
+            This is a `Temporary query` && is considered very slow.
+            It is meant for debugging during development.
+
+            Because db.query returns a promise, return value of res
+            will be undefined if trying to assign return value outside of 
+            the class. This is why we have written an async version above
+            in order to work with the result after the promise resolves
+            outside of the class.
+        */
+        /** @type {Boolean} */
+        isCaseSensitive=isCaseSensitive||false;
+        /** @type {String} */
+        regex=isCaseSensitive?new RegExp(regex):new RegExp(regex,'i');
+        db.query((doc,emit)=>{
+            if(regex.test(doc['input-question'])){
+                emit(doc['input-question'],doc._rev);
+            }},
+            {
+                include_docs:true,
+                descending:true,
+            }
+        ).then(res=>{
+            console.log(res.rows);
+            console.log(res);
+            // return res; => undefined bc db.query is async and unresolved @time of assignment.
+        }).catch(err=>{
+            console.log(err);
+        });
+    }
+}
--- static/js/dbActions.js
+++ static/js/dbActions.js
@@ -1,15 +1,30 @@
+
+// Create flashcard
 document.getElementById('create-btn').addEventListener('click',function(e){
     // identical evListener in html on line 1411.
     // https://pouchdb.com/api.html#save_attachment
     e.preventDefault();
 
-    const pouch = new Pouch(e.target.form);
-    // pouch.logForm();
-    // pouch.logFormData();
-    // console.log(pouch.numInputs);
-    // console.log(pouch.doc);
-    // pouch.addFlashcard();
-    pouch.print();
-    pouch.pprint();
+    const flashcard = new Flashcard(e.target.form);
+    // flashcard.logForm();
+    flashcard.logFormData();
+    // console.log(flashcard.numInputs);
+    // console.log(flashcard.doc);
+    flashcard.addFlashcard();
+    console.log(flashcard.lastSavedCard); // doesn't work as expected. empty obj.
+    // flashcard.print();
+    flashcard.pprint();
 
+    // const testDB=new Query(db);
+    // testDB.getAll(); // doesn't show currently saved flashcard here, but does when manually doing so in console. May not have refreshed yet.
+
+    // Memory clean up:
+    revokeImageURL();
+    revokeAudioURL();
+    revokeVideoURL();
 });
+
+const testDB=new Query(db);
+// testDB._question_sync('1'); // only prints result.
+
+testDB._question_async('1').then(res=> console.log(res));
