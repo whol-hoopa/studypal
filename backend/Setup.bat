d55fd5bd86393b5f66b06d519c735416d51fd55f
Bob Khounsombath <bob.khounsombath@gmail.com>
2024-04-25 17:42:11 -0700

    MVC-like file structure change; integration of front|back end

--- backend/Setup.bat
+++
@@ -1,4 +0,0 @@
-pip install -r "requirements.txt"
-docker create --name studypal -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password couchdb:3.3.3
-docker start studypal
-uvicorn backend:studypal_backend --reload --port 8080
